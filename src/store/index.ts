709a57c1aea3d2f0979ab79eea1ebd539ce74f5c
NIKI443 <140906614+NIKI443@users.noreply.github.com>
2025-02-19 09:32:19 +0300

    build

--- src/store/index.ts
+++
@@ -1,8 +0,0 @@
-export { useChatStore } from './useChatStore'
-export { useAuthStore } from './useAuthStore'
-export { useDialogStore } from './useDialogStore'
-export { useMessageStore } from './useMessageStore'
-export { useSocketMessageStore } from './useSocketMessageStore'
-
-
-
