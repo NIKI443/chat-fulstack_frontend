c3cdec9659da7f9fd01ab2bea6cf0da134b96e95
NIKI443 <140906614+NIKI443@users.noreply.github.com>
2025-02-21 19:46:38 +0300

    add vercelPreset

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,9 +1,11 @@
 import type { Config } from "@react-router/dev/config";
+import { vercelPreset } from '@vercel/react-router/vite'
 
 export default {
-  appDirectory: "src",
-  ssr: false,
-  async prerender() {
-    return ["/", "/register", "/home"];
-  },
-} satisfies Config;
+	appDirectory: 'src',
+	ssr: false,
+	presets: [vercelPreset()],
+	async prerender() {
+		return ['/', '/register', '/home']
+	},
+} satisfies Config
